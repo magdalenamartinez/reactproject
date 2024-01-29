f94efc5603163869deaf83886d827acb13416dcb
magdalenamartinez <magdalena.martinez@alu.uhu.es>
2024-01-29 19:56:39 +0100

    afteruser2

--- react1/src/Components/dropDown/dropDownItem.js
+++ react1/src/Components/dropDown/dropDownItem.js
@@ -6,7 +6,7 @@ function DropDownItem(props) {
     return(
         <li className="dropDownItem">
             <i className={props.icon}></i>
-            <a href={props.link} className="link_drop">{props.text}</a>
+            <Link to={props.link} className="link_drop">{props.text}</Link>
         </li>
     );
 
