3a766a4fe0823649d92cad62719a3998c804aee0
magdalenamartinez <magdalena.martinez@alu.uhu.es>
2024-01-29 09:43:17 +0100

    change in CORS

--- server/app.js
+++ server/app.js
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(express.json());
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://frontend-empleoinclusivo.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.set('port', process.env.PORT || 5000);
 
