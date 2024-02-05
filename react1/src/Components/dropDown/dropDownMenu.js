6e43feab2e278856223f47d01b54f3c7f3ab64b5
magdalenamartinez <magdalena.martinez@alu.uhu.es>
2024-02-05 20:41:05 +0100

    dropdown

--- react1/src/Components/dropDown/dropDownMenu.js
+++ react1/src/Components/dropDown/dropDownMenu.js
@@ -3,19 +3,20 @@ import "../../css/dropdown.css";
 import DropDownItem from "./dropDownItem.js";
 import { useNavigate } from 'react-router-dom';
 
-function DropDownMenu({ openclass, profile, logout, favoritos }) {
+function DropDownMenu({ openclass, profile, logout, favoritos}) {
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        localStorage.removeItem(logout);
-        navigate('/');
-    };
+      logout();
+      navigate("/");
+  }
+
     
         
   return (
     <div className={openclass} id="drop_menu">
       <ul className="list_drop_down">
-      <DropDownItem link={profile} icon={"icons_class fa-solid fa-user"} text="Ver Perfil" />
+      <DropDownItem link={profile} icon={"icons_class fa-solid fa-user"} text="Ver Perfil"/>
       <DropDownItem link={favoritos} icon={"icons_class fa-solid fa-heart"} text="Ver Favoritos" />
         <li className="dropDownItem">
             <i className="icons_class fa-solid fa-right-from-bracket"></i>
